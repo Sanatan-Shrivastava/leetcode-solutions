b1b5048777262b5a34b8351ccfb8948d03f4cb3a
Sanatan Shrivastava <2018kucp1096@iiitkota.ac.in>
2022-12-06 01:56:55 -0600

    412. Fizz Buzz

---
+++ 412. Fizz Buzz.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> output = new ArrayList<>();
+        if (n == 0) {
+            return output;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                output.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                output.add("Fizz");
+            } else if (i % 5 == 0) {
+                output.add("Buzz");
+            } else {
+                output.add("" + i);
+            }
+        }
+
+        return output;
+    }
+}
